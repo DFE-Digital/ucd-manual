87f1d92c5f49122e07a6cfe5353a941ffda6ebde
Andy Jones <andy.jones@digital.education.gov.uk>
2023-04-24 08:39:35 +0100

    Updated logo for assidife

--- app/config.js
+++ app/config.js
@@ -14,8 +14,9 @@ module.exports = {
 
   mtype: 'design',
 
-  githubrepo: 'https://github.com/DFE-Digital/ucd-manual'
+  githubrepo: 'https://github.com/DFE-Digital/ucd-manual',
   
+  assetPath: process.env.assetPath
 };
 
 
